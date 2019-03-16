a8015c3bcbc2ef67b951b903f22497fd888b5c0b
Alex <48605031+youfekdup@users.noreply.github.com>
2019-03-16 20:10:14 +0500

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,4 +6,6 @@ MODDIR=${0%/*}
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
 
-sysctl -p
+chmod 777 /proc/sys/net/ipv4/ip_no_pmtu_disc
+sysctl -w net.ipv4.ip_no_pmtu_disc=0
+chmod 555 /proc/sys/net/ipv4/ip_no_pmtu_disc
